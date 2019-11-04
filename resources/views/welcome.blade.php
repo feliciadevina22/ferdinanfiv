535f0bf13b472d44ddc0ee12a817c4e01c6cab70
shelly <shellystarss@gmail.com>
2019-11-04 12:06:40 +0700

    w/ node.js

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    FERDINANFIV
                 </div>
 
                 <div class="links">
@@ -93,6 +93,7 @@
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://vapor.laravel.com">Vapor</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="portofolio">Portfolio</a>
                 </div>
             </div>
         </div>
